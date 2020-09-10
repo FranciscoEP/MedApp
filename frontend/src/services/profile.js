13ceba9a08b63ed6e5b0d6296db77e1bd5d83ca5
FranciscoEP <francisco.eugp@gmail.com>
2020-09-09 19:06:13 -0500

    trying changes

--- frontend/src/services/profile.js
+++ frontend/src/services/profile.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'https://immense-journey-87400.herokuapp.com'
+const baseURL = 'https://immense-journey-87400.herokuapp.com/'
 
 // const baseURL = 'http://localhost:3000/'
 
